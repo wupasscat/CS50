5b7bf35294ce979086e6983bc4d97af4d82dff21
wupasscat <wupasscat10@gmail.com>
2021-03-31 16:02:23 -0400

    Update cash.py
    I don't understand why this doesn't work but here you go

--- cash.py
+++ cash.py
@@ -1,17 +1,30 @@
-while(True):
+while (True):
     quarter = 25
     dime = 0
     nickel = 5
     penny = 1
     coins = 0
     changeInput = input("Change owed: ")
-    usrInput = changeInput.strip("$")
-    floatInput = float(usrInput)
-    if floatInput > 0.00:
-        coins = floatInput * 100
-        cents = 100 - coins
-        while(cents >= 0):
-            cents -= 25
-            print("Cents: ", cents)
+    inputCheck = changeInput.strip(".")
+    if inputCheck.isnumeric():
+        usrInput = changeInput.strip("$")
+        floatInput = float(usrInput)
+        if floatInput > 0.00:
+            cents = int(floatInput * 100)
+            while(cents >= 25):
+                cents -= 25
+                coins += 1
+            while(cents >= 10):
+                cents -= 10
+                coins += 1
+            while(cents >= 5):
+                cents -= 5
+                coins += 1
+            while(cents >= 1):
+                cents -= 1
+                coins += 1
             print("Coins: ", coins)
-        
+        else:
+            print("(!) Please input a number above 0")
+    else:
+        print("(!) Please input a valid number")
